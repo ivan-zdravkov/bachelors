91627a54e57e9331b4f99e3536064bf4eb111bbe
Ivan Zdravkov <IvanZdravkovBG@gmail.com>
2014-08-08 11:23:09 +0900

    Connection String Setup

--- DiplomProject/WebSite/Models/IdentityModels.cs
+++ DiplomProject/WebSite/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace WebSite.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("DBConnection")
         {
         }
     }
