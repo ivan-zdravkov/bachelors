9d372f8e761793eace9219e12be346b4517ecbe4
Ivan Zdravkov <IvanZdravkovBG@gmail.com>
2014-08-17 13:48:33 +0300

    Fixing the Social Buttons outlook

--- DiplomProject/WebSite/App_Start/BundleConfig.cs
+++ DiplomProject/WebSite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace WebSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/social-buttons-3.css"));
         }
     }
 }
